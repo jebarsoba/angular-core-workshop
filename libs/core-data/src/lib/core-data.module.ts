0d8689c503b35f9f260b8fbd35eea590d8fb16e9
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2020-04-05 09:29:46 -0300

    Covered until 'Creating a Service'

--- libs/core-data/src/lib/core-data.module.ts
+++ libs/core-data/src/lib/core-data.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ProjectsService } from './projects/projects.service';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  providers: [
+    ProjectsService
+  ]
 })
 export class CoreDataModule {}
