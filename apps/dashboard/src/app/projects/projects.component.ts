500d3a817c5b6fb7d214a253516fdb61e4260bd3
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2020-04-09 09:39:20 -0300

    Covered until 'ngModel Name Property'

--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -40,7 +40,26 @@ export class ProjectsComponent implements OnInit {
   }
 
   saveProject(project) {
-    console.log('SAVING PROJECT', project); 
+    if (!project.id)
+      this.createProject(project);
+    else
+      this.updateProject(project);
+  }
+
+  createProject(project) {
+    this.projectsService.create(project)
+      .subscribe(() => {
+        this.getProjects();
+        this.resetProject();
+      });
+  }
+
+  updateProject(project) {
+    this.projectsService.update(project)
+      .subscribe(() => {
+        this.getProjects();
+        this.resetProject();
+      });
   }
 
   deleteProject(project) {
