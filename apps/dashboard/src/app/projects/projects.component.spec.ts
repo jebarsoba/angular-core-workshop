700469052407eeb505fdb014d3aef2a69a007a71
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2020-04-11 16:19:43 -0300

    Covered until 'Testing Wrap-up'

--- apps/dashboard/src/app/projects/projects.component.spec.ts
+++ apps/dashboard/src/app/projects/projects.component.spec.ts
@@ -1,41 +1,85 @@
 import { HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@workshop/material';
-
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectsComponent } from './projects.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ProjectsComponent', () => {
+  // Create my local test members
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
+  let de: DebugElement;
+  // let projectsService: ProjectsService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  // const mockProjectsService = {
+
+  // };
+
+  // Instantiate test bed
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
       declarations: [
         ProjectsComponent,
         ProjectsListComponent,
         ProjectDetailsComponent
       ],
+      // providers: [
+      //   {
+      //     provide: ProjectsService,
+      //     useValue: mockProjectsService
+      //   }
+      // ],
       imports: [
         MaterialModule,
         FormsModule,
         HttpClientModule,
         BrowserAnimationsModule
       ]
-    })
-      .compileComponents();
-  }));
+    }).createComponent(ProjectsComponent);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectsComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    // projectsService = de.injector.get(ProjectsService);
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have a primary color of `red`', () => {
+    expect(component.primaryColor).toBe('red');
+  });
+
+  it('should select a project', () => {
+    const emptyProject = {
+      id: null,
+      title: '',
+      details: '',
+      percentComplete: 0,
+      approved: false,
+    };
+
+    component.selectProject(emptyProject);
+
+    expect(component.selectedProject).toBe(emptyProject);
+  });
+
+  it('should desplay primaryColor', () => {
+    const h1 = de.query(By.css('h1'));
+
+    expect(h1.nativeElement.innerText).toBe('red');
+  });
+
+  it('should update h1 to new primaryColor', () => {
+    const h1 = de.query(By.css('h1'));
+
+    component.primaryColor = 'black';
+    fixture.detectChanges();
+
+    expect(h1.nativeElement.innerText).toBe('black');
   });
 });
